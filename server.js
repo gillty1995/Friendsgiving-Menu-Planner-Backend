e33a4aa2396504ee85749cd682fee7d71b7c6c4c
gillty <gooee@mac.com>
2024-11-18 07:52:31 -0600

    update server.js

--- server.js
+++ server.js
@@ -31,10 +31,7 @@ app.use(
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI, {})
   .then(() => {
     console.log("MongoDB connected successfully");
   })
