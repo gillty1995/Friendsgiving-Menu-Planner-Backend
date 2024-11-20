4345da9da6514f07ec9779ae11008b3a5232c109
gillty <gooee@mac.com>
2024-11-20 14:52:56 -0600

    update server

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.get("/", (req, res) => {
 
 // Server listen
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
